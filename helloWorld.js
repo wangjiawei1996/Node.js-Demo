ba28f3d2179be511b24ee122165f137b2c062131
王嘉巍 <906789088@qq.com>
2018-10-21 00:39:27 +0800

    feat:server-build

--- helloWorld.js
+++ helloWorld.js
@@ -1,6 +1,19 @@
 var http = require('http')
 http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type': 'text/plain'});
-    res.end('Hello world');
+    var path = req.url.replace(/\/?(?:\?.*)?$/,'').toLowerCase();
+    switch(path) {
+        case '':
+            res.writeHead(200,{'Content-Type': 'text/plain'});
+            res.end('Homepage');
+            break;
+        case '/about':
+            res.writeHead(200,{'Content-Type': 'text/plain'});
+            res.end('About');
+            break;
+        default:
+            res.writeHead(404,{'Content-Type': 'text/plain'});
+            res.end('Not Found');
+            break;
+    }
 }).listen(3000);
-console.log('Server started on localhost:3000;press ctrl-c to terminate...')
+console.log('Server started on localhost:3000;press Ctrl-c to terminate...');
