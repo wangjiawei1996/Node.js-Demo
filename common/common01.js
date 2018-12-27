c5224b31ae590af552c153b79ffbf43f45c410c9
王嘉巍 <906789088@qq.com>
2018-12-27 23:35:57 +0800

    feat:silly-datetime

--- common/common01.js
+++ common/common01.js
@@ -1,10 +1,13 @@
+var sd = require('silly-datetime')
 var http = require('http');
 
 var config = require('./config.js')
 
 var app = http.createServer(function(req,res){
   res.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
-  res.write('Hello Node.js')
+
+  var d = sd.format(new Date(),'YYYY-MM-DD ');
+  res.write('Hello Node.js' + d);
   res.end();
 })
 app.listen(8003,'127.0.0.1')
