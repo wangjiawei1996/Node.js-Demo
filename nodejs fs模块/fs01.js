9544a332384609efa0aeda2a009c2c370ae48167
王嘉巍 <906789088@qq.com>
2019-01-01 22:39:24 +0800

    feat: explored fs function

--- nodejs fs模块/fs01.js
+++ nodejs fs模块/fs01.js
@@ -27,3 +27,66 @@ fs.writeFile('t.txt','你好node.js',function(err){
 	}
 	console.log('写入成功')
 })
+
+//fs.appendFile 追加文件
+fs.appendFile('t1.txt','这是写入的内容',function(err){
+	if (err){
+		console.log(err);
+		return false
+	}
+	console.log('写入成功')
+})
+
+//fs.readFile 读取文件
+fs.readFile('index.txt',function(err,data){
+	if (err) {
+		console.log(err)
+		return false
+	}
+	console.log(data.toString)
+})
+
+//fs.readdir 读取目录
+fs.readdir('html',function(err,data){
+	if (err) {
+		console.log(err)
+		return false
+	}
+	console.log(data)
+})
+
+//fs.rename 重命名
+//1.改名
+fs.rename('html/index.html','html/news.html',function(err){
+	if (err) {
+		console.log(err);
+		return false;
+	}
+	console.log('修改名字成功')
+})
+//2.剪切文件
+fs.rename('html/style.css','html/css/basic.css',function(err){
+	if (err) {
+		console.log(err);
+		return false;
+	}
+	console.log('剪切名字成功')
+})
+
+//fs.rmdir 删除目录
+fs.rmdir('js',function(err){
+	if (err) {
+		console.log(err);
+		return false;
+	}
+	console.log('删除目录成功')
+})
+
+//fs.unlink 删除文件 
+fs.unlink('index.txt',function(err){
+	if (err) {
+		console.log(err);
+		return false;
+	}
+	console.log('删除文件成功')
+})
