d0cec3a46e4b62bae8ba76b99927af96165831eb
王嘉巍 <906789088@qq.com>
2019-03-31 17:06:44 +0800

    feat: by router-express get and post catch data

--- nodejsrouter/06router_express_module.js
+++ nodejsrouter/06router_express_module.js
@@ -1,8 +1,21 @@
 var http = require('http')
+var ejs = require('ejs')
 var app = require('./model/express-route')
 
 http.createServer(app).listen(3000)
 
 app.get('/login', function(req, res) {
+  console.log('login')
+  ejs.renderFile('views/form.ejs',{},function(err, data) {
+    res.end(data)
+  })
   res.end('login')
+})
+app.post('/dologin', function(req,res) {
+  console.log(req.body)
+  res.send("<script>alert('登录成功');history.back();</script>")
+})
+app.get('register', function(req, res) {
+  console.log('register')
+  res.send('register')
 })
--- nodejsrouter/model/express-route.js
+++ nodejsrouter/model/express-route.js
@@ -1,13 +1,35 @@
 var url = require('url')
+function changeRes(res) {
+  res.send = function(data) {
+    res.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"})
+    res.end(data)
+  }
+}
 var Server = function() {
   var G = this
+  this._get={}
+  this._post={}
   var app = function(req, res) {
+    changeRes(res)
     var pathname = url.parse(req.url).pathname
     if (!pathname.endsWith('/')){
       pathname = pathname + '/'
     }
-    if (G[pathname]) {
-      G[pathname](req, res)  //执行注册的方法
+    var method = req.method.toLowerCase()
+    if (G['_' + method][pathname]) {
+      if (method=='post') {
+        var postStr = ''
+        req.on('data', function(chunk) {
+          postStr += chunk
+        })
+        req.on('end',function(err,chunk) {
+          //res.end(postStr);
+          req.body = postStr
+          G['_' + method][pathname](req, res)
+        })
+      } else {
+        G['_' + method][pathname](req, res)
+      }
     } else {
       res.end('no router')
     }
@@ -20,7 +42,16 @@ var Server = function() {
     if (!string.startsWith('/')) {
       string = '/' + string
     }
-    G[string] = callback
+    G._get[string] = callback
+  }
+  app.post = function(string, callback) {
+    if (!string.endsWith('/')){
+      string = string + '/'
+    }
+    if (!string.startsWith('/')) {
+      string = '/' + string
+    }
+    G._post[string] = callback
   }
   return app
 }
